d5eddd20c42d66375170eb4a9c514354ecb2bcb5
yiopxz <dnstj2123@icloud.com>
2025-05-20 20:27:17 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,33 +9,40 @@ interface Calculator {
 class KDG_Calculator implements Calculator {
     private int a;
     private int b;
+
     public KDG_Calculator(int a, int b) {
         this.a = a;
         this.b = b;
     }
 
     public void add() {
-        System.out.println((a+b));
+        System.out.println("Addition: " + (a + b));
     }
+
     public void multiply() {
-        System.out.println((a*b));
+        System.out.println("Multiplication: " + (a * b));
     }
+
     public void subtract() {
-        System.out.println((a-b));
+        System.out.println("Subtraction: " + (a - b));
     }
 }
 
 public class Main {
     public static void main(String[] args) {
         Scanner stdin = new Scanner(System.in);
-        System.out.print();
+        
+        System.out.print("Enter first number: ");
         int a = stdin.nextInt();
-        System.out.print();
+
+        System.out.print("Enter second number: ");
         int b = stdin.nextInt();
 
         Calculator calc = new KDG_Calculator(a, b);
         calc.add();
         calc.multiply();
         calc.subtract();
+
+        stdin.close();  // 스캐너 리소스 정리
     }
 }
