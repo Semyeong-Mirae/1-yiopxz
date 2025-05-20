30d7d371c40adfeb96a01a4bf8166e3793fa03d3
yiopxz <dnstj2123@icloud.com>
2025-05-20 20:39:29 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,25 +7,27 @@ public class Main {
         int num2 = stdin.nextInt();
 
         YEB_Calculator yc = new YEB_Calculator();
-        System.out.println(yc.add(num1,num2));
-        System.out.println(yc.multiply(num1,num2));
-        System.out.println(Calculator.subtract(num1,num2));
+        System.out.println(yc.add(num1, num2));
+        System.out.println(yc.multiply(num1, num2));
+        System.out.println(Calculator.subtract(num1, num2));
     }
 }
 
-
 interface Calculator {
     int add(int a, int b);
+
     default int multiply(int a, int b) {
-        return a*b;
+        return a * b;
     }
+
     static int subtract(int a, int b) {
-        return a-b;
+        return a - b;
     }
 }
 
 class YEB_Calculator implements Calculator {
     @Override
     public int add(int a, int b) {
-        return a+b;
+        return a + b;
     }
+}
